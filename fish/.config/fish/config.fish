eef52d096d43d6a669bf8b9ed5a1ddf13d598bee
Tyler Varnum <tylervarnum9@gmail.com>
2025-11-30 22:15:32 -0500

    did things idk

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,3 +8,5 @@ starship init fish | source
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin /home/tylerv/.ghcup/bin $PATH # ghcup-env
+
+set -gx EDITOR helix
